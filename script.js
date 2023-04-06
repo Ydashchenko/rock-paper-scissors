3c20e0301650ea6f7b7a70f2d8be515a2bc3375c
madjonga <madjonga123@gmail.com>
2023-04-06 14:15:10 +0300

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice () {
+    let answers = ["Rock", "Paper", "Scissors"]
+    let choice = Math.floor(0 + Math.random() * (2 + 1 - 0))
+    return answers[choice]
+}
+
+alert(getComputerChoice())
