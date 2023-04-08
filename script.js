ff6756c9be368affdd1b48c6d558a2fd03760cdb
madjonga <madjonga123@gmail.com>
2023-04-08 13:10:03 +0300

    Create playRound function

--- script.js
+++ script.js
@@ -4,4 +4,44 @@ function getComputerChoice () {
     return answers[choice]
 }
 
-alert(getComputerChoice())
+function playRound (playerSelection, computerSelection) {
+    /* if player selection = computer selection
+    then "Draw!" */
+    if (playerSelection === computerSelection) {
+        console.log("Draw!")
+    }
+    else {
+        switch (playerSelection) {
+            case "Rock":
+                if (computerSelection === "Paper") {
+                    console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`)
+                }
+                else {
+                    console.log(`You Win! ${playerSelection} beats ${computerSelection}!`)
+                }
+                break
+            case "Paper":
+                if (computerSelection === "Scissors") {
+                    console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`)
+                }
+                else {
+                    console.log(`You Win! ${playerSelection} beats ${computerSelection}!`)
+                }
+                break
+            case "Scissors":
+                if (computerSelection === "Rock") {
+                    console.log(`You Lose! ${computerSelection} beats ${playerSelection}!`)
+                }
+                else {
+                    console.log(`You Win! ${playerSelection} beats ${computerSelection}!`)
+                }
+                break
+            default:
+                console.log("Give me Rock, Paper os Scissors!")
+        }
+    }
+}
+
+const playerSelection = "Rock"
+const computerSelection = getComputerChoice()
+playRound(playerSelection, computerSelection)
